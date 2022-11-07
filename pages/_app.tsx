5b9d6babfafa89b881caabf0f145bee2b7dd964b
northfacegawd <chuck5732590@gmail.com>
2022-11-08 00:39:39 +0900

    chore: react query 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
