81e8796cf491c501db377ef20fd94c9ae51fadc5
northfacegawd <chuck5732590@gmail.com>
2022-11-07 22:41:10 +0900

    chore: test library추가 및 설정

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@pages/(.*)$': '<rootDir>/pages/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+    '^@hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^@models/(.*)$': '<rootDir>/models/$1',
+    '^@mocks/(.*)$': '<rootDir>/mocks/$1',
+    '^@styles/(.*)$': '<rootDir>/styles/$1',
+    '^@public/(.*)$': '<rootDir>/public/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ tests/page/home.test.tsx
@@ -0,0 +1,14 @@
+import Home from '@pages/index';
+import { render, screen } from '@testing-library/react';
+
+describe('<Home />', () => {
+  beforeEach(() => {
+    render(<Home />);
+  });
+
+  it('renders component', () => {
+    const home = screen.getByText('Next.js!');
+
+    expect(home).toBeInTheDocument();
+  });
+});
