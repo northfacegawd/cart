e23c8e92f368d26f54d5bce017a7b22c7c66d9d0
northfacegawd <chuck5732590@gmail.com>
2022-11-08 22:49:37 +0900

    feat: 간단한 header 추가

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,7 +1,38 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
-import { GlobalHeader } from './index.style';
+import ShoppingCart from '@components/icons/shopping-cart';
+
+import {
+  GlobalHeader,
+  HeaderLink,
+  HeaderSection,
+  HeaderTopNav,
+} from './index.style';
 
 export default function Header() {
-  return <GlobalHeader>Header</GlobalHeader>;
+  const { asPath } = useRouter();
+
+  return (
+    <GlobalHeader>
+      <HeaderSection>
+        <HeaderTopNav>
+          <Link href="/">
+            <HeaderLink>29CM</HeaderLink>
+          </Link>
+          <Link href="/products">
+            <HeaderLink active={asPath.startsWith('/products')}>
+              쇼핑하러가기
+            </HeaderLink>
+          </Link>
+          <Link href="/cart">
+            <HeaderLink active={asPath.startsWith('/cart')}>
+              <ShoppingCart />
+            </HeaderLink>
+          </Link>
+        </HeaderTopNav>
+      </HeaderSection>
+    </GlobalHeader>
+  );
 }
