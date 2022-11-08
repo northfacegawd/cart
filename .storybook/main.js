51439f794c2505fce581f0791b85189e99ceddf7
northfacegawd <chuck5732590@gmail.com>
2022-11-08 22:12:26 +0900

    feat: 절대경로를 사용할 수 있도록 웹팩 설정 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     '../pages/**/*.stories.@(js|jsx|ts|tsx)',
@@ -12,5 +14,25 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, '..'),
+      'node_modules',
+      'styles',
+    ];
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@pages': path.resolve(__dirname, '../components'),
+      '@components': path.resolve(__dirname, '../components'),
+      '@public': path.resolve(__dirname, '../assets'),
+      '@lib': path.resolve(__dirname, '../lib'),
+      '@hooks': path.resolve(__dirname, '../hooks'),
+      '@models': path.resolve(__dirname, '../models'),
+      '@styles': path.resolve(__dirname, '../styles'),
+    };
+
+    return config;
   },
 };
