ee90e457b2fb0668f4ea3b85f50a71a0d28de15f
northfacegawd <chuck5732590@gmail.com>
2022-11-08 19:46:40 +0900

    chore: storybook, testing library eslint plugin 추가

--- tests/page/home.test.tsx
+++ tests/page/home.test.tsx
@@ -2,11 +2,9 @@ import Home from '@pages/index';
 import { render, screen } from '@testing-library/react';
 
 describe('<Home />', () => {
-  beforeEach(() => {
-    render(<Home />);
-  });
-
   it('renders component', () => {
+    render(<Home />);
+
     const home = screen.getByText('Next.js!');
 
     expect(home).toBeInTheDocument();
