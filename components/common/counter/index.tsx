a73c2053f9d355e4d6afa9a3557955877851c87c
northfacegawd <chuck5732590@gmail.com>
2022-11-10 22:11:18 +0900

    refactor: 사용자가 원하는 min, max를 받을 수 있도록 리팩토링

--- components/common/counter/index.tsx
+++ components/common/counter/index.tsx
@@ -6,18 +6,22 @@ interface CounterProps {
   count: number;
   onIncrease: () => void;
   onDecrease: () => void;
+  min?: number;
+  max?: number;
 }
 
 export default function Counter({
   count,
   onDecrease,
   onIncrease,
+  min,
+  max,
 }: CounterProps) {
   return (
     <CounterWrapper>
       <CounterButton
         onClick={onDecrease}
-        disabled={count <= 0}
+        disabled={count <= min!}
         type="button"
         aria-label="decrease-button"
       >
@@ -25,6 +29,7 @@ export default function Counter({
       </CounterButton>
       <CountInput aria-label="count" readOnly value={count} />
       <CounterButton
+        disabled={count >= max!}
         onClick={onIncrease}
         type="button"
         aria-label="increase-button"
--- hooks/useCounter.tsx
+++ hooks/useCounter.tsx
@@ -1,7 +1,15 @@
 import { useCallback, useState } from 'react';
 
-export function useCounter() {
-  const [count, setCount] = useState<number>(0);
+interface UserCounterOptions {
+  min?: number;
+  max?: number;
+}
+
+export function useCounter(
+  defaultValue = 0,
+  options: UserCounterOptions = { max: 100, min: 0 },
+) {
+  const [count, setCount] = useState<number>(defaultValue);
 
   const onIncrease = useCallback(() => {
     setCount((prev) => prev + 1);
@@ -11,5 +19,5 @@ export function useCounter() {
     setCount((prev) => prev - 1);
   }, []);
 
-  return { count, onIncrease, onDecrease };
+  return { count, onIncrease, onDecrease, ...options };
 }
