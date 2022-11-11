1de39f086224dcfe8da5b517e6f893b57fce03b9
northfacegawd <chuck5732590@gmail.com>
2022-11-12 01:14:51 +0900

    test: useMapState hook 테스트코드 작성

---
+++ tests/hooks/useMapState.test.ts
@@ -0,0 +1,24 @@
+import { useMapState } from '@hooks/useMapState';
+import { act, renderHook } from '@testing-library/react';
+
+describe('useMapState hook', () => {
+  it('state test', () => {
+    const { result } = renderHook(() => useMapState());
+    act(() => result.current[1].onAdd('test', 'This is useMapState test'));
+    expect(result.current[0].get('test')).toBe('This is useMapState test');
+    act(() => result.current[1].onUpdate('test', 'update!'));
+    expect(result.current[0].get('test')).toBe('update!');
+    act(() => result.current[1].onDelete('test'));
+    expect(result.current[0].get('test')).toBeUndefined();
+    act(() => result.current[1].onAdd('test1', 'test1'));
+    act(() => result.current[1].onAdd('test2', 'test2'));
+    act(() => result.current[1].onClear());
+    expect(result.current[0].size).toBe(0);
+
+    // act(() => result.current.onIncrease());
+    // expect(result.current.count).toBe(1);
+    // act(() => result.current.onDecrease());
+    // act(() => result.current.onDecrease());
+    // expect(result.current.count).toBe(-1);
+  });
+});
