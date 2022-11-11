e20b96520fd0b63443223931280bf1a4c6c50cee
northfacegawd <chuck5732590@gmail.com>
2022-11-11 23:19:21 +0900

    feat: size를 비교하기 위한 isSame 함수 구현

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,8 @@
 export const numberIntoPrice = (num: number): string => {
   return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 };
+
+export const isSame = <T extends any>(...rest: T[]): boolean => {
+  const criteia = rest[0];
+  return rest.every((val) => val === criteia);
+};
