ea6442b21dc5b43b5c742f432a4dc0679b32b9aa
northfacegawd <chuck5732590@gmail.com>
2022-11-13 12:37:57 +0900

    feat: 최종 결제 금액 산정 로직 구현

--- components/amount/index.tsx
+++ components/amount/index.tsx
@@ -3,7 +3,12 @@ import { useQuery } from 'react-query';
 import { fetchCoupons } from 'requests/coupon';
 
 import CouponModal from '@components/coupon/modal';
-import { numberIntoPrice } from '@lib/utils';
+import {
+  convertMapIntoArray,
+  getTotal,
+  isSame,
+  numberIntoPrice,
+} from '@lib/utils';
 import { Coupon } from '@models/coupon.model';
 import useStore from '@store/index';
 
@@ -22,11 +27,47 @@ export default function Amount() {
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon>();
   const { data } = useQuery('coupons', fetchCoupons);
 
-  const totalAmount = useMemo(() => {
-    return [...selectedCartList]
-      .map(([, { count, price }]) => count * price)
-      ?.reduce((a, b) => a + b, 0);
-  }, [selectedCartList, data]);
+  const { totalAmount, discountAmount } = useMemo(() => {
+    if (!selectedCartList.size) return { totalAmount: 0, discountAmount: 0 };
+    // 선택된 카트들이 담겨 있는 맵을 배열로 변환
+    const convertedArray = convertMapIntoArray(selectedCartList);
+    // 쿠폰 사용이 가능한 상품들
+    const availableList = convertedArray.filter(
+      (cart) => cart.availableCoupon !== false,
+    );
+    // 쿠폰 사용이 가능한 상품들의 총 합
+    const availableTotal = getTotal(
+      availableList.map(({ price, count }) => price * count),
+    );
+    // 할인금액 초기화
+    let discount = 0;
+    // 할인 쿠폰 종류에 따라 계산을 다르게
+    if (selectedCoupon?.discountRate) {
+      discount = availableTotal / selectedCoupon.discountRate;
+    } else if (selectedCoupon?.discountAmount) {
+      discount = selectedCoupon?.discountAmount ?? discount;
+    }
+    // 선택된 상품과 쿠폰 사용이 가능한 상품의 갯수가 같으면 여기서 리턴
+    if (isSame(convertedArray.length, availableList.length)) {
+      return { totalAmount: availableTotal, discountAmount: discount };
+    }
+    // 쿠폰을 사용하지 못 하는 상품들
+    const unavailableList = convertedArray.filter(
+      (cart) => cart.availableCoupon === false,
+    );
+    // 선택된 상품과 쿠폰 사용이 불가능한 상품의 갯수가 같으면 할인금액 초기화
+    if (isSame(unavailableList.length, convertedArray.length)) {
+      discount = 0;
+    }
+    // 쿠폰 사용이 불가능한 상품들의 총 합
+    const unavailableTotal = getTotal(
+      unavailableList.map(({ count, price }) => count * price),
+    );
+    return {
+      totalAmount: unavailableTotal + availableTotal,
+      discountAmount: discount,
+    };
+  }, [selectedCartList, selectedCoupon]);
 
   const onSelectCoupon = (coupon: Coupon) => setSelectedCoupon(coupon);
 
@@ -46,11 +87,13 @@ export default function Amount() {
         </AmountBox>
         <AmountBox width="30%">
           <Subject>쿠폰할인</Subject>
-          <Price>0</Price>
+          <Price>{discountAmount}원</Price>
         </AmountBox>
         <AmountBox width="35%">
           <Subject>총 결제금액</Subject>
-          <Price total>{numberIntoPrice(totalAmount)}원</Price>
+          <Price total>
+            {numberIntoPrice(totalAmount - discountAmount, true)}원
+          </Price>
         </AmountBox>
       </AmountWrapper>
       <CouponModal
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,17 @@
-export const numberIntoPrice = (num: number): string => {
+export const numberIntoPrice = (num: number, ceil?: boolean): string => {
+  if (ceil) Math.ceil(num);
   return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 };
 
 export const isSame = <T extends any>(...rest: T[]): boolean => {
   const criteia = rest[0];
   return rest.every((val) => val === criteia);
+};
+
+export const convertMapIntoArray = <K, V>(target: Map<K, V>) => {
+  return [...target].map(([, value]) => value);
+};
+
+export const getTotal = (target: Array<number>): number => {
+  return target.reduce((a, b) => a + b, 0);
 };
